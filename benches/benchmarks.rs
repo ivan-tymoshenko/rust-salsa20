b07c4f86a79476317a5767f5373f7a03fe3618a1
Ivan Timoshenko <bugagashenkj@gmail.com>
2019-08-15 16:42:41 +0300

    Add generate with overflow
    PR-URL: https://github.com/bugagashenkj/salsa20/pull/2

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -34,10 +34,36 @@ fn generate_1Mb(c: &mut Criterion) {
     }));
 }
 
+fn generate_1Kb_without_overflow(c: &mut Criterion) {
+    let mut salsa20 = Salsa20::new(&[0; 16], &[0; 8], 0);
+    let mut buffer = [0; 1024];
+
+    c.bench_function("generate 1Kb without overflow", move |b| b.iter(|| {
+        salsa20.generate(black_box(&mut buffer[0..256]));
+        salsa20.generate(black_box(&mut buffer[256..512]));
+        salsa20.generate(black_box(&mut buffer[512..768]));
+        salsa20.generate(black_box(&mut buffer[768..1024]));
+    }));
+}
+
+fn generate_1Kb_with_overflow(c: &mut Criterion) {
+    let mut salsa20 = Salsa20::new(&[0; 16], &[0; 8], 0);
+    let mut buffer = [0; 1024];
+
+    c.bench_function("generate 1Kb with overflow", move |b| b.iter(|| {
+        salsa20.generate(black_box(&mut buffer[0..7]));
+        salsa20.generate(black_box(&mut buffer[7..259]));
+        salsa20.generate(black_box(&mut buffer[259..938]));
+        salsa20.generate(black_box(&mut buffer[938..1024]));
+    }));
+}
+
 criterion_group!(
     benches,
     generate_64b,
     generate_1Kb,
     generate_1Mb,
+    generate_1Kb_with_overflow,
+    generate_1Kb_without_overflow
 );
 criterion_main!(benches);
