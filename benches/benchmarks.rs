d756114b9677545e78956a1f4c04546ee7361f3f
Ivan Timoshenko <bugagashenkj@gmail.com>
2019-08-16 18:30:17 +0300

    Remove unused benchmarks
    PR-URL: https://github.com/bugagashenkj/salsa20/pull/7

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -7,16 +7,7 @@ use salsa20::Salsa20;
 use criterion::Criterion;
 use criterion::black_box;
 
-fn generate_64b(c: &mut Criterion) {
-    let mut salsa20 = Salsa20::new(&[0; 16], &[0; 8], 0);
-    let mut buffer = [0; 64];
-
-    c.bench_function("generate 64b", move |b| b.iter(|| {
-        salsa20.generate(black_box(&mut buffer))
-    }));
-}
-
-fn generate_1Kb(c: &mut Criterion) {
+fn generate_1_kb(c: &mut Criterion) {
     let mut salsa20 = Salsa20::new(&[0; 16], &[0; 8], 0);
     let mut buffer = [0; 1024];
 
@@ -25,28 +16,7 @@ fn generate_1Kb(c: &mut Criterion) {
     }));
 }
 
-fn generate_1Mb(c: &mut Criterion) {
-    let mut salsa20 = Salsa20::new(&[0; 16], &[0; 8], 0);
-    let mut buffer = [0; 1024 * 1024];
-
-    c.bench_function("generate 1Mb", move |b| b.iter(|| {
-        salsa20.generate(black_box(&mut buffer))
-    }));
-}
-
-fn generate_1Kb_without_overflow(c: &mut Criterion) {
-    let mut salsa20 = Salsa20::new(&[0; 16], &[0; 8], 0);
-    let mut buffer = [0; 1024];
-
-    c.bench_function("generate 1Kb without overflow", move |b| b.iter(|| {
-        salsa20.generate(black_box(&mut buffer[0..256]));
-        salsa20.generate(black_box(&mut buffer[256..512]));
-        salsa20.generate(black_box(&mut buffer[512..768]));
-        salsa20.generate(black_box(&mut buffer[768..1024]));
-    }));
-}
-
-fn generate_1Kb_with_overflow(c: &mut Criterion) {
+fn generate_1_kb_with_overflow(c: &mut Criterion) {
     let mut salsa20 = Salsa20::new(&[0; 16], &[0; 8], 0);
     let mut buffer = [0; 1024];
 
@@ -60,10 +30,7 @@ fn generate_1Kb_with_overflow(c: &mut Criterion) {
 
 criterion_group!(
     benches,
-    generate_64b,
-    generate_1Kb,
-    generate_1Mb,
-    generate_1Kb_with_overflow,
-    generate_1Kb_without_overflow
+    generate_1_kb,
+    generate_1_kb_with_overflow
 );
 criterion_main!(benches);
