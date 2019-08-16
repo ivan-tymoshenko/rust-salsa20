aeed1623a2ff2ba354263ac5553a3c9e4962210f
bugagashenkj <bugagashenkj@gmail.com>
2019-08-17 00:56:15 +0300

    Add speed param to output of benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,40 +1,49 @@
-#[macro_use]
-
 extern crate criterion;
 extern crate salsa20;
 
 use salsa20::Salsa20;
-use criterion::Criterion;
-use criterion::black_box;
+use criterion::*;
 
 fn encrypt_1_kb(c: &mut Criterion) {
     let mut salsa20 = Salsa20::new(&[0; 16], &[0; 8], 0);
     let mut buffer = [0; 1024];
 
-    c.bench_function("encrypt 1Kb", move |b| b.iter(|| {
-        salsa20.encrypt(black_box(&mut buffer))
-    }));
+    c.bench(
+        "encrypt",
+        Benchmark::new(
+            "1Kb", move |b| b.iter(|| salsa20.encrypt(black_box(&mut buffer)))
+        ).throughput(Throughput::Bytes(1024))
+    );
 }
 
 fn generate_1_kb(c: &mut Criterion) {
     let mut salsa20 = Salsa20::new(&[0; 16], &[0; 8], 0);
     let mut buffer = [0; 1024];
 
-    c.bench_function("generate 1Kb", move |b| b.iter(|| {
-        salsa20.generate(black_box(&mut buffer))
-    }));
+    c.bench(
+        "generate",
+        Benchmark::new(
+            "1Kb", move |b| b.iter(|| salsa20.generate(black_box(&mut buffer)))
+        ).throughput(Throughput::Bytes(1024))
+    );
 }
 
 fn generate_1_kb_with_overflow(c: &mut Criterion) {
     let mut salsa20 = Salsa20::new(&[0; 16], &[0; 8], 0);
-    let mut buffer = [0; 1024 * 1024];
+    let mut buffer = [0; 1024];
 
-    c.bench_function("generate 1Mb with overflow", move |b| b.iter(|| {
-        salsa20.generate(black_box(&mut buffer[0..7]));
-        salsa20.generate(black_box(&mut buffer[7..259]));
-        salsa20.generate(black_box(&mut buffer[259..938]));
-        salsa20.generate(black_box(&mut buffer[938..1024]));
-    }));
+    c.bench(
+        "generate with overflow",
+        Benchmark::new(
+            "1Kb",
+            move |b| b.iter(|| {
+                salsa20.generate(black_box(&mut buffer[0..7]));
+                salsa20.generate(black_box(&mut buffer[7..259]));
+                salsa20.generate(black_box(&mut buffer[259..938]));
+                salsa20.generate(black_box(&mut buffer[938..1024]));
+            })
+        ).throughput(Throughput::Bytes(1024))
+    );
 }
 
 criterion_group!(
