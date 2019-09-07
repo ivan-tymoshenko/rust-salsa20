fe65a55f5c6fe76cf1ea0d20e57141c18154a8ec
Ivan Timoshenko <bugagashenkj@gmail.com>
2019-09-07 22:26:03 +0300

    Fix Salsa20::new args in benchmarks
    PR-URL: https://github.com/bugagashenkj/rust-salsa20/pull/56

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -5,7 +5,7 @@ use rust_salsa20::{Salsa20, Key::Key16};
 use criterion::*;
 
 fn encrypt_1_kb(c: &mut Criterion) {
-    let mut salsa20 = Salsa20::new(&Key16([1; 16]), &[0; 8], 0);
+    let mut salsa20 = Salsa20::new(Key16([1; 16]), [0; 8], 0);
     let mut buffer = [0; 1024];
 
     c.bench(
@@ -17,7 +17,7 @@ fn encrypt_1_kb(c: &mut Criterion) {
 }
 
 fn generate_1_kb(c: &mut Criterion) {
-    let mut salsa20 = Salsa20::new(&Key16([2; 16]), &[0; 8], 0);
+    let mut salsa20 = Salsa20::new(Key16([2; 16]), [0; 8], 0);
     let mut buffer = [0; 1024];
 
     c.bench(
@@ -29,7 +29,7 @@ fn generate_1_kb(c: &mut Criterion) {
 }
 
 fn generate_1_kb_with_overflow(c: &mut Criterion) {
-    let mut salsa20 = Salsa20::new(&Key16([3; 16]), &[0; 8], 0);
+    let mut salsa20 = Salsa20::new(Key16([3; 16]), [0; 8], 0);
     let mut buffer = [0; 1024];
 
     c.bench(
