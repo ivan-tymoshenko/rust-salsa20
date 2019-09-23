4f1b109beed78d989a2c6e4308a3d98031c9bf26
bugagashenkj <bugagashenkj@gmail.com>
2019-09-24 00:19:47 +0300

    Add unit tests for utils
    PR-URL: https://github.com/bugagashenkj/rust-salsa20/pull/62

--- src/utils.rs
+++ src/utils.rs
@@ -10,8 +10,41 @@ pub(super) fn u8_to_u32(bytes: &[u8], u32_slice: &mut [u32]) {
     }
 }
 
-pub(super) fn xor_from_slice(to: &mut[u8], from: &[u8]) {
+pub(super) fn xor_from_slice(to: &mut [u8], from: &[u8]) {
     for (to_byte, from_byte) in to.iter_mut().zip(from.iter()) {
         *to_byte ^= from_byte;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn u8_to_u32_test() {
+        test(&[0, 0, 0, 0], &[0]);
+        test(&[1, 0, 0, 0], &[1]);
+        test(&[1, 2, 3, 4], &[67305985]);
+        test(&[1, 2, 3, 4, 5], &[67305985]);
+
+        fn test(bytes: &[u8], expected_values: &[u32]) {
+            let mut values = [0];
+            u8_to_u32(&bytes, &mut values);
+            assert_eq!(values, expected_values);
+        }
+    }
+
+    #[test]
+    fn xor_from_slice_test() {
+        test(&mut [0, 0, 0, 0], &[0, 0, 0, 0], &[0, 0, 0, 0]);
+        test(&mut [0, 0, 0, 1], &[0, 0, 0, 0], &[0, 0, 0, 1]);
+        test(&mut [1, 0, 1, 0], &[1, 1, 0, 0], &[0, 1, 1, 0]);
+        test(&mut [1, 2, 3, 4], &[5, 6, 7, 8], &[4, 4, 4, 12]);
+        test(&mut [1, 2, 3, 4, 0, 0], &[5, 6, 7, 8], &[4, 4, 4, 12, 0, 0]);
+
+        fn test(to: &mut [u8], from: &[u8], expected: &[u8]) {
+            xor_from_slice(to, from);
+            assert_eq!(to, expected);
+        }
     }
 }
